72ee65fe55c94867f7ac5a6307514df848c5484e
unknown <fcttmk@yandex.ru>
2021-08-30 17:01:00 +0700

    add modal form

--- src/components/EmployeesItem.jsx
+++ src/components/EmployeesItem.jsx
@@ -1,8 +1,53 @@
+import { useState } from 'react';
 import styles from './../css/employees_list.module.css'
 
 export default function EmployeesItem({firstName, lastName}){
+
+    const [modalEditVisible, setModalEditVisible] = useState(false);
+    const [modalDeleteVisible, setModalDeleteVisible] = useState(false);
+
+    function EditEmp() {
+        return (
+            <div className={styles.modalWrap}>
+                <div className={styles.modal}>
+                    <img src="closeIcon.png" alt="close" onClick={() => setModalEditVisible(false)} />
+                    <div className={styles.modal_header}>
+                        <h3>Редактирование сотрудника</h3>
+                    </div>
+                    <div className={styles.modal_inputs}>
+                        <input type="text" name="firstName" />
+                        <input type="text" name="lastName" />
+                        <button>Сохранить</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
+    function RemoveEmp(){
+        return (
+            <div className={styles.modalWrap}>
+                <div className={styles.modal}>
+                    <img src="closeIcon.png" alt="close" onClick={() => setModalDeleteVisible(false)} />
+                    <div className={styles.modal_header}>
+                        <h3>Вы действительно хотите удалить сотрудника?</h3>
+                    </div>
+                    <div className={styles.modal_inputs}>
+            
+                        <button style={{backgroundColor: 'red'}}>Удалить</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className={styles.listItem}>
+
+            {modalDeleteVisible ? <RemoveEmp /> : null}
+            {modalEditVisible ? <EditEmp /> : null}
            
                 <span>
                     <img className={styles.userIcon} src="userIcon.png" alt="" />
@@ -12,7 +57,8 @@ export default function EmployeesItem({firstName, lastName}){
                     {lastName}
                 </span>
                 <span>
-                    Закр.редакт.
+                        <img src="editIcon.png" alt="edit" onClick={() => setModalEditVisible(true)} />
+                        <img src="deleteIcon.png" alt="png" onClick={() => setModalDeleteVisible(true)} />
                 </span>
             
         </div>
--- src/components/EmployeesList.jsx
+++ src/components/EmployeesList.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './../css/employees_list.module.css';
 import EmployeesItem from './EmployeesItem';
-import { useState } from 'react';
 
 export default function EmployeesList() {
 
+    const [modalVisible, setModalVisible] = useState(false);
     const [persons, setPersons] = useState();
 
     useEffect(() => {
@@ -13,11 +13,32 @@ export default function EmployeesList() {
             method: 'get',
             url: 'http://localhost:3000/person'
         })
-        .then((res) => setPersons(res.data))
-    },[])
+            .then((res) => setPersons(res.data))
+    }, [])
+
+    function AddingEmp() {
+        return (
+            <div className={styles.modalWrap}>
+                <div className={styles.modal}>
+                    <img src="closeIcon.png" alt="close" onClick={() => setModalVisible(false)} />
+                    <div className={styles.modal_header}>
+                        <h3>Создание сотрудника</h3>
+                    </div>
+                    <div className={styles.modal_inputs}>
+                        <input type="text" name="firstName" />
+                        <input type="text" name="lastName" />
+                        <button>Сохранить</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
 
     return (
         <div className={styles.container}>
+            {modalVisible ? <AddingEmp /> : null}
             <div className={styles.table_header}>
                 <div className={styles.header_items}>
 
@@ -36,14 +57,14 @@ export default function EmployeesList() {
 
             <ul className={styles.employees_table}>
 
-                {persons === undefined ? null : 
-                    persons.map((person) => <li> <EmployeesItem 
-                                                    firstName={person.firstName}
-                                                    lastName={person.lastName}  /> 
-                                            </li> )}
-               
-                
+                {persons === undefined ? null :
+                    persons.map((person) =>
+                        <li> <EmployeesItem
+                            firstName={person.firstName}
+                            lastName={person.lastName} />
+                        </li>)}
             </ul>
+            <button onClick={() => setModalVisible(true)} className={styles.add_btn}>Добавить сотрудника</button>
         </div>
     )
 }
